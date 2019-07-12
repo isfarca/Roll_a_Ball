536a48bb89d0040a713a92b4a4d5f18e8fc6aecc
Fethi Isfarca <fisfarca@googlemail.com>
2019-07-12 11:39:39 +0200

    Portrait mode.

--- Roll_a_Ball/Assets/_Completed-Game/Scripts/PlayerController.cs
+++ Roll_a_Ball/Assets/_Completed-Game/Scripts/PlayerController.cs
@@ -16,6 +16,8 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
 	private int count;
 
+    private bool isFlat = true;
+
 	// At the start of the game..
 	void Start ()
 	{
@@ -32,24 +34,34 @@ public class PlayerController : MonoBehaviour {
 		winText.text = "";
 	}
 
-	// Each physics step..
-	void FixedUpdate ()
-	{
-		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+    // Each physics step..
+    void FixedUpdate()
+    {
+        // Set some local float variables equal to the value of our Horizontal and Vertical Inputs
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-		// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        // Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-		// Add a physical force to our Player rigidbody using our 'movement' Vector3 above, 
-		// multiplying it by 'speed' - our public player speed that appears in the inspector
-		rb.AddForce (movement * speed);
-	}
+        // Add a physical force to our Player rigidbody using our 'movement' Vector3 above, 
+        // multiplying it by 'speed' - our public player speed that appears in the inspector
+        rb.AddForce(movement * speed);
 
-	// When this game object intersects a collider with 'is trigger' checked, 
-	// store a reference to that collider in a variable named 'other'..
-	void OnTriggerEnter(Collider other) 
+        Vector3 tilt = Input.acceleration;
+
+        if (isFlat)
+        {
+            tilt = Quaternion.Euler(360, 0, 0) * tilt;
+        }
+
+        rb.AddForce(tilt);
+        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan);
+    }
+
+    // When this game object intersects a collider with 'is trigger' checked, 
+    // store a reference to that collider in a variable named 'other'..
+    void OnTriggerEnter(Collider other) 
 	{
 		// ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
 		if (other.gameObject.CompareTag ("Pick Up"))
